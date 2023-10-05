34cba1746e7a089d0dbe2f9b41acb091be6e7249
KevinTeguh <kevinteguh64@gmail.com>
2023-10-05 19:42:42 +0700

    test

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(MiddlewareLogRequest);
 app.use(express.json());
 app.use(express.static("public"));
+app.use("/home", express.static("public/index.html"));
 app.use('/api/users', usersRoutes);
 app.use('/api/dokter', dokterRoutes);
 app.use('/api/book', bookRoutes);
