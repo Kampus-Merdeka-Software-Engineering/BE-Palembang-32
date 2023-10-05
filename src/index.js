d9eb5a3f32ff74cef29e5cd52107fefd3bf0cd67
KevinTeguh <kevinteguh64@gmail.com>
2023-10-05 19:34:50 +0700

    integrasi

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(MiddlewareLogRequest);
 app.use(express.json());
+app.use(express.static("public"));
 app.use('/api/users', usersRoutes);
 app.use('/api/dokter', dokterRoutes);
 app.use('/api/book', bookRoutes);
